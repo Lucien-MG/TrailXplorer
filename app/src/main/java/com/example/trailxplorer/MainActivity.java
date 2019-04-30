b4a4c6edf2e2a35654280b8fd2d4f81843c4bba0
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2019-04-30 11:30:20 +0200

    All modifs

--- app/src/main/java/com/example/trailxplorer/MainActivity.java
+++ app/src/main/java/com/example/trailxplorer/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.trailxplorer;
 
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LocationManager lm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GpsHelper gpsHelper = new GpsHelper(this,
+                MainActivity.this,
+                (TextView) findViewById(R.id.tv_lat),
+                (TextView) findViewById(R.id.tv_long));
+
+        lm = gpsHelper.addLocationListener();
     }
 }
