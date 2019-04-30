8f76c3125301424f4e4f1596a1bf9bf2e6dab5d5
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2019-04-30 14:44:01 +0200

    Add internet to gps

--- app/src/main/java/com/example/trailxplorer/GpsHelper.java
+++ app/src/main/java/com/example/trailxplorer/GpsHelper.java
@@ -14,19 +14,31 @@ import android.location.LocationManager;
 
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+
+// Widget import:
 import android.widget.TextView;
 import android.widget.Toast;
 
+// Java import:
+import java.util.Map;
+
 public class GpsHelper {
-    // private fields of the classprivate
+    // private fields of the class
     private TextView tv_lat, tv_long;
     private LocationManager lm;
-    private int RequestAnswer = 0;
+    private int RequestAnswer;
 
+    // Contain message send by gps:
+    private Toast toast;
+
+    // Contain activity context:
     private Context ActivityContext;
     private Activity A_Activity;
 
-    public GpsHelper(Context context, Activity activity) {
+    // UI connection:
+    private Map<String, TextView> tv_uiInterface;
+
+    public GpsHelper(Context context, Activity activity, Map<String, TextView> uiInterface) {
         // Get application context:
         ActivityContext = context;
         A_Activity = activity;
@@ -37,76 +49,95 @@ public class GpsHelper {
         // List of permissions needed:
         String[] perms = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
                                       Manifest.permission.ACCESS_FINE_LOCATION,
-                                      Manifest.permission.ACCESS_NETWORK_STATE};
+                                      Manifest.permission.INTERNET};
 
         // Request Permission to acces to the GPS:
         ActivityCompat.requestPermissions(activity, perms, RequestAnswer);
+
+        // Get interface:
+        tv_uiInterface = uiInterface;
     }
 
     // public method that will add a location listener to the location manager
     public LocationManager addLocationListener() {
         if (ActivityCompat.checkSelfPermission(ActivityContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(ActivityContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Toast t = Toast.makeText(ActivityContext, "No authorization", Toast.LENGTH_LONG);
-            t.show();
+
+            toast = Toast.makeText(ActivityContext, "The application has not the permission to use GPS.", Toast.LENGTH_LONG);
+            toast.show();
+
             return null;
         }
 
-        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Toast t = Toast.makeText(ActivityContext, "GPS enable", Toast.LENGTH_LONG);
-            t.show();
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            toast = Toast.makeText(ActivityContext, "WARNING: GPS Disable", Toast.LENGTH_LONG);
+            toast.show();
         }
 
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, new LocationListener() {
+        if (!lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            toast = Toast.makeText(ActivityContext, "WARNING: NETWORK Location Disable", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 0, new LocationListener() {
+
             @Override
             public void onLocationChanged(Location location) {
                 // the location of the device has changed so update the
                 // textviews to reflect this
-                Toast t = Toast.makeText(ActivityContext, "test: " + location.getLatitude(), Toast.LENGTH_LONG);
-                t.show();
+                toast = Toast.makeText(ActivityContext, "test: " + location.getLatitude(), Toast.LENGTH_LONG);
+                toast.show();
+
+                updateUI(location);
             }
 
             @Override
             public void onProviderDisabled(String provider) {
-                // if GPS has been disabled then update the textviews to reflect
-                // this
+                // if GPS has been disabled then update the TextViews to reflect
                 if (provider == LocationManager.GPS_PROVIDER) {
                     //tv_lat.setText("Latitude");
                     //tv_long.setText("Longitude");
+                }
+
+                if (provider == LocationManager.NETWORK_PROVIDER) {
+
                 }
             }
 
             @Override
             public void onProviderEnabled(String provider) {
-                // if there is a last known location then set it on the
-                // textviews
+                // if there is a last known location then set it on the Text View:
                 if (provider == LocationManager.GPS_PROVIDER) {
                     if (ActivityCompat.checkSelfPermission(ActivityContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                             ActivityCompat.checkSelfPermission(ActivityContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        Toast t = Toast.makeText(ActivityContext, "No authorization", Toast.LENGTH_LONG);
-                        t.show();
+
+                        toast = Toast.makeText(ActivityContext, "The application has not the permission to use GPS.", Toast.LENGTH_LONG);
+                        toast.show();
+
                         return;
                     }
 
-                    Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                    if (l != null) {
-                        tv_lat.setText("Latitude: " + l.getLatitude());
-                        tv_long.setText("Longitude: " + l.getLongitude());
+                    if (location != null) {
+                        updateUI(location);
+                    }
+                }
+
+                if (provider == LocationManager.NETWORK_PROVIDER) {
+                    if (ActivityCompat.checkSelfPermission(ActivityContext, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED &&
+                            ActivityCompat.checkSelfPermission(ActivityContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                        toast = Toast.makeText(ActivityContext, "The application has not the permission to use Location.", Toast.LENGTH_LONG);
+                        toast.show();
+
+                        return;
+                    }
+
+                    Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                    if (location != null) {
+                        updateUI(location);
                     }
                 }
             }
@@ -118,5 +149,11 @@ public class GpsHelper {
         });
 
         return lm;
+    }
+
+    private void updateUI(Location location) {
+        //tv_uiInterface.get("time_run").setText(location.getSpeed() + " km/h");
+        tv_uiInterface.get("current_speed").setText((int)location.getSpeed() + "km/h");
+        tv_uiInterface.get("current_altitude").setText((int)location.getAltitude() + " m");
     }
 }
--- app/src/main/java/com/example/trailxplorer/MainActivity.java
+++ app/src/main/java/com/example/trailxplorer/MainActivity.java
@@ -5,17 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private LocationManager lm;
+
+    // All text view useful for GPS:
+    Map<String, TextView> tv_uiInterface = new HashMap<String, TextView>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GpsHelper gpsHelper = new GpsHelper(this, MainActivity.this);
+        initUiInterface();
+
+        GpsHelper gpsHelper = new GpsHelper(this, MainActivity.this, tv_uiInterface);
 
         lm = gpsHelper.addLocationListener();
+    }
+
+    private void initUiInterface() {
+        tv_uiInterface.put("time_run", (TextView) findViewById(R.id.timeRun));
+        tv_uiInterface.put("current_speed", (TextView) findViewById(R.id.curSpeed));
+        tv_uiInterface.put("current_altitude", (TextView) findViewById(R.id.curAlt));
     }
 }
