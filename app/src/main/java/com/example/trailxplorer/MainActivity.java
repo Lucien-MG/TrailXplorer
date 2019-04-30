8903c0667b6c0a3fdba101704add64b764cdb789
Lenchanteur42 <merlin.schoose@gmail.com>
2019-04-30 15:17:13 +0200

    First perso Toolbar

--- app/src/main/java/com/example/trailxplorer/MainActivity.java
+++ app/src/main/java/com/example/trailxplorer/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.trailxplorer;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -20,11 +23,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initializing the Toolbar.
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         initUiInterface();
 
         GpsHelper gpsHelper = new GpsHelper(this, MainActivity.this, tv_uiInterface);
 
         lm = gpsHelper.addLocationListener();
+    }
+
+    //Initializes the Toolbar.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     private void initUiInterface() {
